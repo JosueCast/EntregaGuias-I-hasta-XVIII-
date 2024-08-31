bf0a104f05de05a5d4962a9d0daa945f3c95f517
JosueCast <126531292+JosueCast@users.noreply.github.com>
2024-08-30 22:46:08 -0600

    Add files via upload

---
+++ GuiaProgra2/CapaConexion/CapaConexion/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace CapaConexion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFiltro = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.dataGridViewImageColumn1 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btModificar = new System.Windows.Forms.Button();
+            this.tboxContacName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.tboxCity = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tboxAddress = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tboxContactTitle = new System.Windows.Forms.TextBox();
+            this.lbContactTitle = new System.Windows.Forms.Label();
+            this.tboxCompanyName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tboxCustomerID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AllowUserToAddRows = false;
+            this.dataGrid.AllowUserToDeleteRows = false;
+            this.dataGrid.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGrid.DefaultCellStyle = dataGridViewCellStyle8;
+            this.dataGrid.Location = new System.Drawing.Point(15, 66);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.ReadOnly = true;
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle9.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle9;
+            this.dataGrid.RowHeadersVisible = false;
+            this.dataGrid.Size = new System.Drawing.Size(623, 393);
+            this.dataGrid.TabIndex = 0;
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(263, 11);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(96, 38);
+            this.btnCargar.TabIndex = 1;
+            this.btnCargar.Text = "Cargar Informacion";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Buscador :";
+            // 
+            // txtFiltro
+            // 
+            this.txtFiltro.Location = new System.Drawing.Point(76, 21);
+            this.txtFiltro.Name = "txtFiltro";
+            this.txtFiltro.Size = new System.Drawing.Size(160, 20);
+            this.txtFiltro.TabIndex = 3;
+            this.txtFiltro.TextChanged += new System.EventHandler(this.txtFiltro_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(778, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "CustomerID";
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(846, 14);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(100, 20);
+            this.txtBuscar.TabIndex = 5;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(781, 66);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(192, 23);
+            this.btnBuscar.TabIndex = 6;
+            this.btnBuscar.Text = "Buscar Cliente";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // dataGridViewImageColumn1
+            // 
+            this.dataGridViewImageColumn1.HeaderText = "Column1";
+            this.dataGridViewImageColumn1.Image = global::CapaConexion.Properties.Resources._3borrar;
+            this.dataGridViewImageColumn1.Name = "dataGridViewImageColumn1";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(660, 420);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(385, 39);
+            this.btnEliminar.TabIndex = 38;
+            this.btnEliminar.Text = "Eliminar usuario ";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btModificar
+            // 
+            this.btModificar.Location = new System.Drawing.Point(888, 372);
+            this.btModificar.Name = "btModificar";
+            this.btModificar.Size = new System.Drawing.Size(157, 23);
+            this.btModificar.TabIndex = 37;
+            this.btModificar.Text = "Modificar";
+            this.btModificar.UseVisualStyleBackColor = true;
+            this.btModificar.Click += new System.EventHandler(this.btModificar_Click);
+            // 
+            // tboxContacName
+            // 
+            this.tboxContacName.Location = new System.Drawing.Point(847, 205);
+            this.tboxContacName.Name = "tboxContacName";
+            this.tboxContacName.Size = new System.Drawing.Size(126, 20);
+            this.tboxContacName.TabIndex = 36;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(758, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 35;
+            this.label5.Text = "ContactName";
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(660, 372);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(183, 23);
+            this.btnInsertar.TabIndex = 34;
+            this.btnInsertar.Text = "Ingresar ";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // tboxCity
+            // 
+            this.tboxCity.Location = new System.Drawing.Point(847, 329);
+            this.tboxCity.Name = "tboxCity";
+            this.tboxCity.Size = new System.Drawing.Size(126, 20);
+            this.tboxCity.TabIndex = 33;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(771, 329);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(24, 13);
+            this.label8.TabIndex = 32;
+            this.label8.Text = "City";
+            // 
+            // tboxAddress
+            // 
+            this.tboxAddress.Location = new System.Drawing.Point(847, 289);
+            this.tboxAddress.Name = "tboxAddress";
+            this.tboxAddress.Size = new System.Drawing.Size(126, 20);
+            this.tboxAddress.TabIndex = 31;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(771, 296);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "Address";
+            // 
+            // tboxContactTitle
+            // 
+            this.tboxContactTitle.Location = new System.Drawing.Point(847, 247);
+            this.tboxContactTitle.Name = "tboxContactTitle";
+            this.tboxContactTitle.Size = new System.Drawing.Size(126, 20);
+            this.tboxContactTitle.TabIndex = 29;
+            // 
+            // lbContactTitle
+            // 
+            this.lbContactTitle.AutoSize = true;
+            this.lbContactTitle.Location = new System.Drawing.Point(762, 250);
+            this.lbContactTitle.Name = "lbContactTitle";
+            this.lbContactTitle.Size = new System.Drawing.Size(64, 13);
+            this.lbContactTitle.TabIndex = 28;
+            this.lbContactTitle.Text = "ContactTitle";
+            // 
+            // tboxCompanyName
+            // 
+            this.tboxCompanyName.Location = new System.Drawing.Point(847, 156);
+            this.tboxCompanyName.Name = "tboxCompanyName";
+            this.tboxCompanyName.Size = new System.Drawing.Size(126, 20);
+            this.tboxCompanyName.TabIndex = 27;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(762, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "CompanyName";
+            // 
+            // tboxCustomerID
+            // 
+            this.tboxCustomerID.Location = new System.Drawing.Point(847, 121);
+            this.tboxCustomerID.Name = "tboxCustomerID";
+            this.tboxCustomerID.Size = new System.Drawing.Size(126, 20);
+            this.tboxCustomerID.TabIndex = 25;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(771, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Customers ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1057, 483);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btModificar);
+            this.Controls.Add(this.tboxContacName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.tboxCity);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tboxAddress);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tboxContactTitle);
+            this.Controls.Add(this.lbContactTitle);
+            this.Controls.Add(this.tboxCompanyName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tboxCustomerID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFiltro);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.dataGrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFiltro;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.DataGridViewImageColumn dataGridViewImageColumn1;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btModificar;
+        private System.Windows.Forms.TextBox tboxContacName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.TextBox tboxCity;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tboxAddress;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tboxContactTitle;
+        private System.Windows.Forms.Label lbContactTitle;
+        private System.Windows.Forms.TextBox tboxCompanyName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tboxCustomerID;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ GuiaProgra2/CapaConexion/CapaConexion/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using DatosLayer;
+using System.Reflection;
+
+
+
+namespace CapaConexion
+{
+    public partial class Form1 : Form
+    {
+        CustomerRepository customersRepository = new CustomerRepository();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            var Customer = customersRepository.ObtenerTodos();
+            dataGrid.DataSource = Customer;
+        }
+        private void txtFiltro_TextChanged(object sender, EventArgs e)
+        {
+            //var filtro = Customers.FindAll(X => X.CompanyName.StartsWith(txtFiltro.Text));
+            //dataGrid.DataSource = filtro;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //DatosLayer.DataBase.ApplicationName = "Programacion 2 ejemplo";
+            //DatosLayer.DataBase.ConnetionTimeout = 30;
+
+            //string cadenaconexion = DatosLayer.DataBase.ConnectionString;
+            //var conn = DatosLayer.DataBase.GetSqlConnection();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+           
+            var cliente = customersRepository.ObtenerPorId(txtBuscar.Text);
+            tboxCustomerID.Text = cliente.CustomerID;
+            tboxCompanyName.Text = cliente.CompanyName;
+            tboxContacName.Text = cliente.ContactName;
+            tboxContactTitle.Text = cliente.ContactTitle;
+            tboxAddress.Text = cliente.Address;
+            tboxCity.Text = cliente.City;
+
+
+
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            var resultado = 0;
+            var nuevoCliente = ObtenerNuevoCliente();
+            if (validarCampoNull(nuevoCliente) == false)
+            {
+                resultado = customersRepository.InsertarCliente(nuevoCliente);
+                MessageBox.Show("Guardado" + "Filas modificadas = " + resultado);
+            }
+            else
+            {
+                MessageBox.Show("Debe completar los campos por favor");
+            }
+            /*
+            if (nuevoCliente.CustomerID == "") {
+                MessageBox.Show("El Id en el usuario debe de completarse");
+               return;    
+            }
+
+            if (nuevoCliente.ContactName == "")
+            {
+                MessageBox.Show("El nombre de usuario debe de completarse");
+                return;
+            }
+            
+            if (nuevoCliente.ContactTitle == "")
+            {
+                MessageBox.Show("El contacto de usuario debe de completarse");
+                return;
+            }
+            if (nuevoCliente.Address == "")
+            {
+                MessageBox.Show("la direccion de usuario debe de completarse");
+                return;
+            }
+            if (nuevoCliente.City == "")
+            {
+                MessageBox.Show("La ciudad de usuario debe de completarse");
+                return;
+            }
+
+            */
+        }
+        // si encautnra un null enviara true de lo caontrario false
+        public Boolean validarCampoNull(Object objeto)
+        {
+
+            foreach (PropertyInfo property in objeto.GetType().GetProperties())
+            {
+                object value = property.GetValue(objeto, null);
+                if ((string)value == "")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        private Customers ObtenerNuevoCliente()
+        {
+            var resultado = 0;
+            var nuevoCliente = new Customers
+            {
+
+                CustomerID = tboxCustomerID.Text,
+                CompanyName = tboxCompanyName.Text,
+                ContactName = tboxContacName.Text,
+                ContactTitle = tboxContactTitle.Text,
+                Address = tboxAddress.Text,
+                City = tboxCity.Text,
+
+
+            };
+
+            return nuevoCliente;
+        }
+
+        private void btModificar_Click(object sender, EventArgs e)
+        {
+            var actualizarCliente = ObtenerNuevoCliente();
+            int actualizadas = customersRepository.ActualizarCliente(actualizarCliente);
+            MessageBox.Show($"Filas actualizadas = {actualizadas}");
+        }
+
+
+
+    }
+}
